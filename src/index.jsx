6a4bdec1015df1849ed3efb3f7ea96cfd58cff12
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-10 14:09:04 +0300

    05 - Redux: Writing a Counter Reducer with Tests

--- src/index.jsx
+++ src/index.jsx
@@ -1,8 +1,41 @@
 import 'babel-polyfill';
-import React from 'react';
-import { render } from 'react-dom';
+import expect from 'expect';
 
-render(
-	<div />,
-	document.getElementById('app')
-);
+function counter (state, action) {
+	if (typeof state === 'undefined') {
+		return 0;
+	}
+	if (action.type === 'INCREMENT') {
+		return state + 1;
+	} else if (action.type === 'DECREMENT') {
+		return state - 1;
+	} else {
+		return state;
+	}
+}
+
+expect(
+	counter(0, { type: 'INCREMENT' })
+).toEqual(1);
+
+expect(
+	counter(1, { type: 'INCREMENT' })
+).toEqual(2);
+
+expect(
+	counter(2, { type: 'DECREMENT' })
+).toEqual(1);
+
+expect(
+	counter(1, { type: 'DECREMENT' })
+).toEqual(0);
+
+expect (
+	counter(1, { type: 'SOMETHING_ELSE' })
+).toEqual(1);
+
+expect (
+	counter(undefined, {})
+).toEqual(0);
+
+console.log('Tests passed!');
