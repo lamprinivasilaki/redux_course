0d3a86a3f711fbb222b4ae7edec230f75f2c779e
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-10 15:27:05 +0300

    07 - Redux: Implementing Store from Scratch

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,4 @@
 import expect from 'expect';
-import { createStore } from 'redux';
 
 const counter = (state = 0, action) => {
 	switch (action.type) {
@@ -12,11 +11,30 @@ const counter = (state = 0, action) => {
 	}
 };
 
-const store = createStore(counter);
+const createStore = (reducer) => {
+	let state;
+	let listeners = [];
 
-// console.log(store.getState());
-// store.dispatch({ type: 'INCREMENT' });
-// console.log(store.getState());
+	const getState = () => state;
+
+	const dispatch = (action) => {
+		state = reducer(state, action);
+		listeners.forEach(listener => listener());
+	};
+
+	const subscribe = (listener) => {
+		listeners.push(listener);
+		return () => {
+			listeners = listeners.filter(l => l !== listener);
+		};
+	};
+
+	dispatch({});
+
+	return { getState, dispatch, subscribe };
+};
+
+const store = createStore(counter);
 
 const render = () => {
 	document.body.innerText = store.getState();
