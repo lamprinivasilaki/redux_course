38296b8e02e515dc2cdeb50ea074acc4c82c7926
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-11 16:53:13 +0300

    10 - Redux: Avoiding Object Mutations with Object.assign() and ...spread

--- src/index.jsx
+++ src/index.jsx
@@ -1,88 +1,50 @@
 import expect from 'expect';
 import deepFreeze from 'deep-freeze';
 
+const toggleTodo = (todo) => {
+	// This way causes mutation, so it cannot be
+	// used with Redux
+	// todo.completed = ! todo.completed;
+	// return todo;
 
-const addCounter = (list) => {
-	// push method causes mutation and it
-	// cannot pass the deepFreeze test
-	// list.push(0);
+	// This way works fine, there is a more efficient
+	// and effective way to achieve it.
+	// return {
+	// 	id: todo.id,
+	// 	text: todo.text,
+	// 	completed: !todo.completed
+	// };
 
-	// concat method works perfectly without mutations
-	// I just replace it with es6 spread operator
-	// return list.concat([0]);
+	// This way works perfectly, but we can do it
+	// using the ES7 object spread operator
+	// return Object.assign({}, todo, {
+	// 	completed: !todo.completed
+	// });
 
-	return [...list, 0];
+	return {
+		...todo,
+		completed: !todo.completed
+	};
 };
 
-const removeCounter = (list, index) => {
-	// splice is a mutative method,
-	// so I cannot use it with Redux
-	// list.splice(index, 1);
+const testToggleTodo = () => {
+	const todoBefore = {
+		id: 0,
+		text: 'Learn Redux',
+		completed: false
+	};
+	const todoAfter = {
+		id: 0,
+		text: 'Learn Redux',
+		completed: true
+	};
 
-	// slice & concat works with Redux,
-	// but I use again ES6 array spread operator
-	// return list
-	// 	.slice(0, index)
-	// 	.concat(list.slice(index + 1));
-
-	return [
-		...list.slice(0, index),
-		...list.slice(index + 1)
-	];
-};
-
-const incrementCounter = (list, index) => {
-	// this is, of course, a mutation
-	// list[index]++;
-
-	// This works fine and without mutations
-	// I replace it with ES6 array spread operator
-	// return list
-	// 	.slice(0, index)
-	// 	.concat(list[index] + 1)
-	// 	.concat(list.slice(index + 1));
-
-	return [
-		...list.slice(0, index),
-		list[index] + 1,
-		...list.slice(index + 1)
-	];
-};
-
-const testAddCounter = () => {
-	const listBefore = [];
-	const listAfter = [0];
-
-	deepFreeze(listBefore);
+	deepFreeze(todoBefore);
 
 	expect(
-		addCounter(listBefore)
-	).toEqual(listAfter);
+		toggleTodo(todoBefore)
+	).toEqual(todoAfter);
 };
 
-const testRemoveCounter = () => {
-	const listBefore = [0, 10, 20];
-	const listAfter = [0, 20];
-
-	deepFreeze(listBefore);
-
-	expect(
-		removeCounter(listBefore, 1)
-	).toEqual(listAfter);
-};
-
-const testIncrementCounter = () => {
-	const listBefore = [0, 10, 20];
-	const listAfter = [0, 11, 20];
-
-	deepFreeze(listBefore);
-
-	expect(
-		incrementCounter(listBefore, 1)
-	).toEqual(listAfter);
-};
-
-testAddCounter();
-testRemoveCounter();
-testIncrementCounter();
+testToggleTodo();
 console.log('All tests passed.');
