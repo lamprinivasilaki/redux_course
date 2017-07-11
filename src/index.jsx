515cd4617ecaaaf1c92c97c02fc03ceeaf2ced92
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-11 15:44:56 +0300

    09 - Redux: Avoiding Array Mutations with concat(), slice(), and ...spread

--- src/index.jsx
+++ src/index.jsx
@@ -1,52 +1,88 @@
-import {createStore} from 'redux';
-import React from 'react';
-import ReactDOM from 'react-dom';
+import expect from 'expect';
+import deepFreeze from 'deep-freeze';
 
-const counter = (state = 0, action) => {
-	switch (action.type) {
-		case 'INCREMENT':
-			return state + 1;
-		case 'DECREMENT':
-			return state - 1;
-		default:
-			return state;
-	}
+
+const addCounter = (list) => {
+	// push method causes mutation and it
+	// cannot pass the deepFreeze test
+	// list.push(0);
+
+	// concat method works perfectly without mutations
+	// I just replace it with es6 spread operator
+	// return list.concat([0]);
+
+	return [...list, 0];
 };
 
-const store = createStore(counter);
+const removeCounter = (list, index) => {
+	// splice is a mutative method,
+	// so I cannot use it with Redux
+	// list.splice(index, 1);
 
-const Counter = ({
-	value,
-	onIncrement,
-	onDecrement
-}) => (
-	<div>
-		<h1>{ value }</h1>
-		<button onClick={onIncrement}>+</button>
-		<button onClick={onDecrement}>-</button>
-	</div>
-);
+	// slice & concat works with Redux,
+	// but I use again ES6 array spread operator
+	// return list
+	// 	.slice(0, index)
+	// 	.concat(list.slice(index + 1));
 
-console.log('root element ', document.getElementById('root'));
-
-const render = () => {
-	ReactDOM.render(
-		<Counter
-			value={store.getState()}
-			onIncrement={() =>
-				store.dispatch({
-					type: 'INCREMENT'
-				})
-			}
-			onDecrement={() =>
-				store.dispatch({
-					type: 'DECREMENT'
-				})
-			}
-		/>,
-		document.getElementById('root')
-	);
+	return [
+		...list.slice(0, index),
+		...list.slice(index + 1)
+	];
 };
 
-store.subscribe(render);
-render();
+const incrementCounter = (list, index) => {
+	// this is, of course, a mutation
+	// list[index]++;
+
+	// This works fine and without mutations
+	// I replace it with ES6 array spread operator
+	// return list
+	// 	.slice(0, index)
+	// 	.concat(list[index] + 1)
+	// 	.concat(list.slice(index + 1));
+
+	return [
+		...list.slice(0, index),
+		list[index] + 1,
+		...list.slice(index + 1)
+	];
+};
+
+const testAddCounter = () => {
+	const listBefore = [];
+	const listAfter = [0];
+
+	deepFreeze(listBefore);
+
+	expect(
+		addCounter(listBefore)
+	).toEqual(listAfter);
+};
+
+const testRemoveCounter = () => {
+	const listBefore = [0, 10, 20];
+	const listAfter = [0, 20];
+
+	deepFreeze(listBefore);
+
+	expect(
+		removeCounter(listBefore, 1)
+	).toEqual(listAfter);
+};
+
+const testIncrementCounter = () => {
+	const listBefore = [0, 10, 20];
+	const listAfter = [0, 11, 20];
+
+	deepFreeze(listBefore);
+
+	expect(
+		incrementCounter(listBefore, 1)
+	).toEqual(listAfter);
+};
+
+testAddCounter();
+testRemoveCounter();
+testIncrementCounter();
+console.log('All tests passed.');
