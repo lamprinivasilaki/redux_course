fdeb19690becbdb417b3d12cdebc79d06234a8df
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-12 15:41:35 +0300

    Moving code includes tests with expect & deepFreeze to todoTests file

---
+++ src/todoTests.jsx
@@ -0,0 +1,67 @@
+// import expect from 'expect';
+// import deepFreeze from 'deep-freeze';
+//
+// const testAddTodo = () => {
+// 	const stateBefore = [];
+// 	const action = {
+// 		type: 'ADD_TODO',
+// 		id: 0,
+// 		text: 'Learn Redux'
+// 	};
+// 	const stateAfter = [
+// 		{
+// 			id: 0,
+// 			text: 'Learn Redux',
+// 			completed: false
+// 		}
+// 	];
+//
+// 	deepFreeze(stateBefore);
+// 	deepFreeze(action);
+//
+// 	expect(
+// 		todos(stateBefore, action)
+// 	).toEqual(stateAfter);
+// };
+//
+// const testToggleTodo = () => {
+// 	const stateBefore = [
+// 		{
+// 			id: 0,
+// 			text: 'Learn Redux',
+// 			completed: false
+// 		},
+// 		{
+// 			id: 1,
+// 			text: 'Go shopping',
+// 			completed: false
+// 		}
+// 	];
+// 	const action = {
+// 		type: 'TOGGLE_TODO',
+// 		id: 1
+// 	};
+// 	const stateAfter = [
+// 		{
+// 			id: 0,
+// 			text: 'Learn Redux',
+// 			completed: false
+// 		},
+// 		{
+// 			id: 1,
+// 			text: 'Go shopping',
+// 			completed: true
+// 		}
+// 	];
+//
+// 	deepFreeze(stateBefore);
+// 	deepFreeze(action);
+//
+// 	expect(
+// 		todos(stateBefore, action)
+// 	).toEqual(stateAfter);
+// };
+//
+// testAddTodo();
+// testToggleTodo();
+// console.log('All tests passed.');
