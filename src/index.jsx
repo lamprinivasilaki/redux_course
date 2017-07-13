bb97f17e4c1f6426040f696cc7deb0b2d55a4994
Lamprini Vasilaki <lamprinivasilaki@gmail.com>
2017-07-13 12:58:42 +0300

    22 - Redux: Extracting Container Components (FilterLink)

--- src/index.jsx
+++ src/index.jsx
@@ -57,13 +57,12 @@ const todoApp = combineReducers ({
 
 const store = createStore(todoApp);
 
-const FilterLink =({
-	filter,
-	currentFilter,
+const Link =({
+	active,
 	children,
 	onClick
 }) => {
-	if(filter === currentFilter) {
+	if(active) {
 		return <span>{children}</span>;
 	}
 
@@ -71,7 +70,7 @@ const FilterLink =({
 		<a href='#'
 			onClick={e => {
 				e.preventDefault();
-				onClick(filter);
+				onClick();
 			}}
 		>
 			{children}
@@ -79,33 +78,57 @@ const FilterLink =({
 	);
 };
 
-const Footer = ({
-	visibilityFilter,
-	onFilterClick
-}) => (
+class FilterLink extends Component {
+	componentDidMount() {
+		this.unsubscribe = store.subscribe(() =>
+			this.forceUpdate()
+		);
+	}
+
+	componentWillUnmount() {
+		this.unsubscribe();
+	}
+
+	render() {
+		const props = this.props;
+		const state = store.getState();
+
+		return (
+			<Link
+				active={
+					props.filter === state.visibilityFilter
+				}
+				onClick={() =>
+					store.dispatch({
+						type: 'SET_VISIBILITY_FILTER',
+						filter: props.filter
+					})
+				}
+			>
+				{props.children}
+			</Link>
+		);
+	}
+}
+
+const Footer = () => (
 	<p>
 		Show:
 		{' '}
 		<FilterLink
 			filter='SHOW_ALL'
-			currentFilter={visibilityFilter}
-			onClick={onFilterClick}
 		>
 			All
 		</FilterLink>
-		{' '}
+		{', '}
 		<FilterLink
 			filter='SHOW_ACTIVE'
-			currentFilter={visibilityFilter}
-			onClick={onFilterClick}
 		>
 			Active
 		</FilterLink>
-		{' '}
+		{', '}
 		<FilterLink
 			filter='SHOW_COMPLETED'
-			currentFilter={visibilityFilter}
-			onClick={onFilterClick}
 		>
 			Completed
 		</FilterLink>
@@ -209,15 +232,7 @@ const TodoApp = ({
 				})
 			}
 		/>
-		<Footer
-			visibilityFilter={visibilityFilter}
-			onFilterClick={filter =>
-				store.dispatch({
-					type: 'SET_VISIBILITY_FILTER',
-					filter
-				})
-			}
-		/>
+		<Footer	/>
 	</div>
 );
 
